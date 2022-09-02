d38bc0b58d200ed206f5fe6b471a6bbc71ec4b62
Irvin Jauregui <ijauregui@horizon.csueastbay.edu>
2022-09-02 10:36:36 -0700

    added routing, syncing master branch

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Home from './Home';
+import About from './About';
+import Create from './Create';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
+
+
+export default class NavBar extends Component {
+  render() {
+    return (
+        
+        <Router>
+            <div>
+                <>
+                    <Navbar bg="dark" variant="dark">
+                        <Container>
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                            <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+                            <Nav.Link as={Link} to={"/create"}>Create</Nav.Link>
+                        </Nav>
+                        </Container>
+                    </Navbar>
+                </>
+            </div>
+
+            <div>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/create" element={<Create />}></Route>
+            </Routes>
+            </div>
+            
+        </Router>
+        
+    )
+  }
+}
